9203b151946fc89958409b25e284b160281325d4
Madusanka Madiligama <83027416+mbabeykoon@users.noreply.github.com>
2024-03-19 00:29:48 -0500

    data ingestion and data preprecessing added

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
-# from uwsoundspeed import logger
-# from uwsoundspeed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from uwsoundspeed.pipeline.stage_02_knn_model_train import ConfigurationManager
 
-# STAGE_NAME = "Data Ingestion stage"
-# if __name__ == '__main__':
-#     try:
-#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#         obj = DataIngestionTrainingPipeline()
-#         obj.main()
-#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-#     except Exception as e:
-#         logger.exception(e)
-#         raise e
-    
-
-# STAGE_NAME = "knn model train"
-# try:
-#     logger.info("*******************")
-#     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
-    
-#     config_manager = ConfigurationManager(config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH)  # Ensure these paths are defined
-#     knn_model_train = KNNModelTrain(config_manager)
-
-#     knn_model_train.load_data()
-#     knn_model_train.prepare_pipeline()
-#     knn_model_train.train_test_split()
-#     knn_model_train.train_and_evaluate()
-#     knn_model_train.save_model()
-
-#     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exce
-from uwsoundspeed import logger
+from uwsoundspeed.logging import logger
 from uwsoundspeed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from uwsoundspeed.pipeline.stage_02_knn_model_train import  ConfigurationManager 
-from uwsoundspeed.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH # Assuming KNNModelTrain is in the same module
+from uwsoundspeed.pipeline.stage_02_data_preprocessing import DataPreprocessorPipeline
+# from uwsoundspeed.pipeline.stage_02_knn_model_train import  ConfigurationManager 
+from uwsoundspeed.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH 
 
 # # Ensure these paths are correctly defined
 # CONFIG_FILE_PATH = 'path/to/your/config.yaml' 
@@ -49,18 +18,25 @@ if __name__ == '__main__':
         data_ingestion_obj.main()  # Assuming this method orchestrates the data ingestion stage
         logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-        # KNN Model Training Stage
-        STAGE_NAME = "KNN Model Train"
-        logger.info("*******************")
+        # Data Preprocessing Stage
+        STAGE_NAME = "Data Preprocessing stage"
         logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
-        config_manager = ConfigurationManager(config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH)
-        knn_model_train = KNNModelTrainPipeline(config_manager)
-        knn_model_train.load_data()
-        knn_model_train.prepare_pipeline()
-        knn_model_train.train_test_split()
-        knn_model_train.train_and_evaluate()
-        knn_model_train.save_model()
+        data_preprocessing_obj = DataPreprocessorPipeline()
+        data_ingestion_obj.main()  # Assuming this method orchestrates the data ingestion stage
         logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+        # # KNN Model Training Stage
+        # STAGE_NAME = "KNN Model Train"
+        # logger.info("*******************")
+        # logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        # config_manager = ConfigurationManager(config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH)
+        # knn_model_train = KNNModelTrainPipeline(config_manager)
+        # knn_model_train.load_data()
+        # knn_model_train.prepare_pipeline()
+        # knn_model_train.train_test_split()
+        # knn_model_train.train_and_evaluate()
+        # knn_model_train.save_model()
+        # logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
     except Exception as e:
         logger.exception(e)
---
+++ src/uwsoundspeed/pipeline/stage_02_data_preprocessing.py
@@ -0,0 +1,25 @@
+from uwsoundspeed.config.configuration import ConfigurationManager
+from uwsoundspeed.components.data_preprocessing import DataPreprocessor
+from uwsoundspeed.logging import logger
+
+
+
+
+
+class DataPreprocessorPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+   
+        config = ConfigurationManager()
+        data_preprocessing_config = config.get_data_preprocessing_config()
+        data_preprocessor = DataPreprocessor(config=data_preprocessing_config)
+        
+        # Preprocess the data and apply PCA
+        # This method now internally reads the data, applies transformations, and PCA
+        X_pca = data_preprocessor.preprocess_and_apply_pca()
+
+        # Save the fitted preprocessor and PCA model for later use
+        data_preprocessor.save(subdir_name="data_preprocessor")
+
