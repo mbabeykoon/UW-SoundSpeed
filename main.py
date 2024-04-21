2fe77858327fc80948d4bc9b4881d9808c250f50
Madusanka Madiligama <83027416+mbabeykoon@users.noreply.github.com>
2024-04-20 23:17:39 -0500

    update main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from uwsoundspeed.logging import logger
 from uwsoundspeed.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from uwsoundspeed.pipeline.stage_02_data_preprocessing import DataPreprocessorPipeline
-# from uwsoundspeed.pipeline.stage_02_knn_model_train import  ConfigurationManager 
+from uwsoundspeed.pipeline.stage_03_knn_model_train import  ConfigurationManager ,
 from uwsoundspeed.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH 
 
 # # Ensure these paths are correctly defined
@@ -25,18 +25,18 @@ if __name__ == '__main__':
         data_preprocessing_obj.main()  # Assuming this method orchestrates the data ingestion stage
         logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-        # # KNN Model Training Stage
-        # STAGE_NAME = "KNN Model Train"
-        # logger.info("*******************")
-        # logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
-        # config_manager = ConfigurationManager(config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH)
-        # knn_model_train = KNNModelTrainPipeline(config_manager)
-        # knn_model_train.load_data()
-        # knn_model_train.prepare_pipeline()
-        # knn_model_train.train_test_split()
-        # knn_model_train.train_and_evaluate()
-        # knn_model_train.save_model()
-        # logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        # KNN Model Training Stage
+        STAGE_NAME = "KNN Model Train"
+        logger.info("*******************")
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        config_manager = ConfigurationManager(config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH)
+        knn_model_train = KNNModelTrainPipeline(config_manager)
+        knn_model_train.load_data()
+        knn_model_train.prepare_pipeline()
+        knn_model_train.train_test_split()
+        knn_model_train.train_and_evaluate()
+        knn_model_train.save_model()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
     except Exception as e:
         logger.exception(e)
